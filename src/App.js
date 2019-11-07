fb843c86563ea2f1f0bdda1ed5d972c6a481d2d6
EditYJ <158392613@qq.com>
2019-11-07 16:11:45 +0800

    3-2 第一个展示型组件-价格列表

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+
+import PriceList from "./components/PriceList";
+
+const items = [
+  {
+    id: 1,
+    title: "去旅游啦",
+    price: 200,
+    date: "2019-06-01",
+    category: {
+      id: 1,
+      name: "旅行",
+      type: "outcome"
+    }
+  },
+  {
+    id: 2,
+    title: "去探险啦",
+    price: 800,
+    date: "2019-06-01",
+    category: {
+      id: 1,
+      name: "旅行",
+      type: "income"
+    }
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PriceList
+        items={items}
+        onModifyItem={item => {
+          alert(item.id);
+        }}
+        onDeleteItem={item => {
+          alert(item.id);
+        }}
+      />
     </div>
   );
 }
